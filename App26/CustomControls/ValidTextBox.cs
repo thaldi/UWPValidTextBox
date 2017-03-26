ae6805a9599d051c741b047083b31f38bcee2c01
Yavuz Selim Ulucan <tigris.58@hotmail.com>
2017-03-26 23:11:39 +0300

    Add project files.

---
+++ App26/CustomControls/ValidTextBox.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace App26.CustomControls
+{
+    public class ValidTextBox : TextBox, INotifyPropertyChanged
+    {
+        public ValidTextBox()
+        {
+            ValidateText();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public static readonly DependencyProperty patternProperty =
+            DependencyProperty.Register("pattern", typeof(string), typeof(ValidTextBox), new PropertyMetadata(string.Empty));
+
+        public string RegexPattern
+        {
+            get
+            {
+                return (string)GetValue(patternProperty);
+            }
+            set
+            {
+                SetValue(patternProperty, value);
+            }
+        }
+
+        internal void ValidateText()
+        {
+            this.TextChanged += ValidTextBox_TextChanged;
+        }
+
+        private void ValidTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.RegexPattern))
+                return;
+
+            SetStyle(CheckInDefaultValues());
+        }
+
+
+        private void SetStyle(bool value)
+        {
+            if (value)
+            {
+                if (Regex.IsMatch(this.Text, GetDefaultRegexValue()))
+                {
+                    this.BorderBrush = new SolidColorBrush(Colors.Purple);
+                }
+                else
+                {
+                    this.BorderBrush = new SolidColorBrush(Colors.Red);
+                }
+            }
+            else
+            {
+                if (Regex.IsMatch(this.Text, this.RegexPattern))
+                {
+                    this.BorderBrush = new SolidColorBrush(Colors.Purple);
+                }
+                else
+                {
+                    this.BorderBrush = new SolidColorBrush(Colors.Red);
+                }
+            }
+        }
+
+        private bool CheckInDefaultValues()
+        {
+            if (!string.IsNullOrEmpty(DefaultRegexList.Where(x => x.Key == this.RegexPattern).Select(z => z.Value).FirstOrDefault()))
+                return true;
+            return false;
+        }
+
+        private string GetDefaultRegexValue()
+        {
+            return DefaultRegexList.Where(y => y.Key == this.RegexPattern).Select(s => s.Value).FirstOrDefault();
+        }
+
+        private static IDictionary<string, string> DefaultRegexList
+        {
+            get
+            {
+                return new Dictionary<string, string>()
+                {
+                    { "EMail", @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$" },
+                    { "PhoneNumber", @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}" },
+                    { "Address", @"([\d|A-Z]*) ([A-Z|a-z| ]*) ([A-Z|a-z]*)" },
+                    { "Url", @"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$" },
+                    { "Hex", @"/^#?([a-f0-9]{6}|[a-f0-9]{3})$/" },
+                    { "IPAddress", @"/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/" },
+                    { "HTML", @"/^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)$/" }
+                };
+            }
+        }
+    }
+}
---
+++ App26/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using App26.CustomControls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App26
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnShowMessage_Click(object sender, RoutedEventArgs e)
+        {
+            await new Windows.UI.Popups.MessageDialog(txtValidBox.Text, "Message").ShowAsync();
+        }
+    }
+}
